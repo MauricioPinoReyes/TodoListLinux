f1678d348030cdae2145f7292c068ac67424ed62
mpino <mauricio.pino.log@gmail.com>
2024-11-28 16:40:05 -0300

    backend de la app ok

--- base/views.py
+++ base/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
-#from django.contrib.auth.forms import UserCreationForm
-#from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-##Usuario creado para pruebas del proyecto
-##Username : jhon password: joh#12371
+## Usuarios creados para pruebas del proyecto
+## Usename: admin password: 1234
+## Username: jhon password: joh#12371
+## Username: maria password: mari%34345543
 
-## MIN 1:21:03
+## MIN 1:35:43
 
 from .models import Task
 
@@ -23,6 +25,25 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+    
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
 
 class TaskList(LoginRequiredMixin,ListView):
     model = Task
@@ -33,6 +54,15 @@ class TaskList(LoginRequiredMixin,ListView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(
+                title__contains=search_input)  # utilizar title__startwith para buscar por la primera letra
+            
+        context['search_input'] = search_input
+
+
         return context
 
 class TaskDetail(LoginRequiredMixin,DetailView):
